a9cdf37fe1270505f502498ef1f4e213e7c2036e
root <1059894903@qq.com>
2020-05-20 23:18:34 +0800

    first commit

--- cloud-consumer-order80/src/main/java/com/tutu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/tutu/springcloud/controller/OrderController.java
@@ -3,10 +3,13 @@ package com.tutu.springcloud.controller;
 import com.tutu.springcloud.entities.CommonResult;
 import com.tutu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author tutu 2020/5/20 19:53
@@ -21,6 +24,8 @@ public class OrderController {
 
     @Resource
     private RestTemplate restTemplate;
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/create")
     public CommonResult<?> create(Payment payment) {
@@ -30,6 +35,19 @@ public class OrderController {
     @GetMapping("/get/{id}")
     public CommonResult<?> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("*****service: " + service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return discoveryClient;
     }
 
 }
--- cloud-provider-payment8001/src/main/java/com/tutu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/tutu/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.tutu.springcloud.entities.Payment;
 import com.tutu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author tutu 2020/5/20 17:08
@@ -20,6 +23,8 @@ public class PaymentController {
     PaymentService paymentService;
     @Value("${server.port}")
     private String serverPort;
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/create")
     public CommonResult<Integer> create(@RequestBody Payment payment) {
@@ -41,5 +46,18 @@ public class PaymentController {
         } else {
             return new CommonResult<>(200, "success,port:" + serverPort, result);
         }
+    }
+
+    @GetMapping("/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("*****service: " + service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return discoveryClient;
     }
 }
